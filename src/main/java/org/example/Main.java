82c0b8e449ae1745e0f1393d2c27c08269418002
haniagibus <hg.gibus@gmail.com>
2024-10-20 17:24:34 +0200

    test branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("BI project");
+        System.out.println("BI project - test branch");
     }
 }
