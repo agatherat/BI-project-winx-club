f74ff36b069f48d585241aa10804645bb9ffedaa
michella <michella.milecka@gmail.com>
2024-11-14 23:54:03 +0100

    Generated script to convert json data about products  into csv file to import products into prestashop.

---
+++ src/main/python/converter_products_csv.py
@@ -0,0 +1,53 @@
+import json
+import pandas as pd
+
+with open('products.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+products_info = []
+#image,price after/bedore tax,?weight and producer?
+
+def save_product(product_name, product_url,category_path,product_description,product_price_after_tax,product_quantity):
+    number_price_after_tax = product_price_after_tax.replace("zł", "").strip()
+    number_price_after_tax = number_price_after_tax.replace(",", ".")
+    price_after_tax_float = float(number_price_after_tax)
+    if product_quantity == "Brak w magazynie":
+        quantity_number = 0
+    else:
+        quantity_number = int(product_quantity.split()[0])
+
+    if not any(prod['URL'] == product_url for prod in products_info):
+        products_info.append({
+            'Name': product_name,
+            'Category': category_path,
+            'URL': product_url,
+            'Description':product_description,
+            'Price after tax':price_after_tax_float,
+            'Quantity':quantity_number
+        })
+
+def create_info_category(category_name, category_data, parent_category_path):
+    if parent_category_path:
+        current_category_path = f"{parent_category_path}/{category_name}"
+    else:
+        current_category_path = category_name
+
+    if "subcategories" in category_data:
+        subcategories = category_data["subcategories"]
+        for subcategory_name, subcategory_data in subcategories.items():
+            create_info_category(subcategory_name, subcategory_data, current_category_path)
+
+    if "products" in category_data:
+        for product in category_data["products"]:
+            save_product(product["name"],product["url"], current_category_path,product['description'],product['price'],product['stock'])
+
+for category_name, category_data in data.items():
+    create_info_category(category_name, category_data, "")
+
+csv_data_products = pd.DataFrame(products_info)
+
+if 'URL' in csv_data_products.columns:
+    csv_data_products = csv_data_products.drop(columns=['URL'])
+
+csv_data_products.to_csv('insert_products.csv', index=False, sep=';')
+
