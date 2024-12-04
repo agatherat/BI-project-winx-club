335df1e77b1db51f5dc417cf5307e2f1a2dcb8f0
agatherat <hg.gibus@gmail.com>
2024-12-04 04:31:06 +0100

    fixed dowloadInvoice

--- src/test/java/PrestashopTestset.java
+++ src/test/java/PrestashopTestset.java
@@ -52,9 +52,6 @@ public class PrestashopTestset extends TestBase {
     public void test_deleteProductsFromCart() {
         pagePrestashop.openPage();
 
-//        pagePrestashop.goToPage("https://localhost/69-one-piece");
-//        pagePrestashop.addProductsToCart("https://localhost/69-one-piece", 3);
-
         pagePrestashop.clickOn_cart();
 
         for (int j = 1; j < 4; j++)
@@ -86,29 +83,9 @@ public class PrestashopTestset extends TestBase {
     public void test_createOrder() {
         pagePrestashop.openPage();
 
-//        pagePrestashop.goToPage("https://localhost/74-star-wars-unlimited");
-//        pagePrestashop.addProductsToCart("https://localhost/74-star-wars-unlimited", 1);
-
         pagePrestashop.clickOn_cart();
 
         pagePrestashop.clickOn_byXpath("//*[@id=\"main\"]/div/div[2]/div/div[2]/div");    //click on button "Przejdź do realzacji zamówień"
-
-//        pagePrestashop.clickOn_byXpath("//*[@id=\"checkout-personal-information-step\"]/div/ul/li[3]/a"); //click on login
-//        pagePrestashop.inputTextField_byId("field-email", "newemail@test");
-//        pagePrestashop.inputTextField_byId("field-password", "password");
-//        pagePrestashop.clickOn_byXpath("//*[@id=\"login-form\"]/footer/button");    //click on login button
-
-
-//        pagePrestashop.clickOn_byXpath("//*[@id=\"customer-form\"]/div/div[1]/div[1]/label[2]/span");   //select gender radiobox
-//        pagePrestashop.inputTextField_byId("field-firstname", "test-name");
-//        pagePrestashop.inputTextField_byId("field-lastname", "test-surname");
-//        pagePrestashop.inputTextField_byId("field-email", "new-email@test");
-//        pagePrestashop.inputTextField_byId("field-password", "password");
-//
-//        pagePrestashop.clickOn_byXpath("//*[@id=\"customer-form\"]/div/div[8]/div[1]/span");    //select checkbox
-//        pagePrestashop.clickOn_byXpath("//*[@id=\"customer-form\"]/div/div[10]/div[1]/span");   //select checkbox
-//
-//        pagePrestashop.clickOn_byXpath("//*[@id=\"customer-form\"]/footer/button"); //click on personal data submit button
 
         pagePrestashop.inputTextField_byId("field-address1", "test-address");
         pagePrestashop.inputTextField_byId("field-postcode", "12-345");
@@ -139,17 +116,27 @@ public class PrestashopTestset extends TestBase {
     @Order(7)
     public void test_downloadInvoice() throws InterruptedException {
         //change order status in admin panel
-        pagePrestashop.goToPage("https://localhost:/admin4577/");
+        pagePrestashop.goToPage("https://localhost/admin4577");
+        pagePrestashop.inputTextField_byId("email", "hania.gibus@gmail.com");
+        pagePrestashop.inputTextField_byId("passwd", "winxclub");
+        pagePrestashop.clickOn_byId("submit_login");
+
+        pagePrestashop.waitForElement_byXpath("//*[@id=\"subtab-AdminParentOrders\"]/a");
         pagePrestashop.clickOn_byXpath("//*[@id=\"subtab-AdminParentOrders\"]/a"); //click on "Zamówienia" menu
         pagePrestashop.clickOn_byXpath("//*[@id=\"subtab-AdminOrders\"]/a");    //click on "Zamówienia" submenu
+
+        pagePrestashop.waitForElement_byXpath("//*[@id=\"order_grid_table\"]/tbody/tr[1]/td[9]/div/button");
         pagePrestashop.clickOn_byXpath("//*[@id=\"order_grid_table\"]/tbody/tr[1]/td[9]/div/button");   //click on toggle
         pagePrestashop.clickOn_byXpath("//*[@id=\"order_grid_table\"]/tbody/tr[1]/td[9]/div/div/button[12]"); //click on "Płatność przyjęta" option
 
-        sleep(3000);
+        sleep(2000);
 
         pagePrestashop.openPage();
+        pagePrestashop.waitForElement_byXpath("//*[@id=\"footer_account_list\"]/li[2]/a");
         pagePrestashop.clickOn_byXpath("//*[@id=\"footer_account_list\"]/li[2]/a"); //click on orders link
         pagePrestashop.clickOn_byXpath("//*[@id=\"content\"]/table/tbody/tr[1]/td[5]/a");   //click on invoice link
+
+        sleep(2000);
     }
 }
 
